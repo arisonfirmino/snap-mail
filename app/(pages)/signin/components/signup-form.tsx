fa2967913a6a42620407c44b88dc1e6d16d372cc
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-12-20 01:27:02 -0300

    feat: adiciona validaçào inicial ao formulário SignUpForm

--- app/(pages)/signin/components/signup-form.tsx
+++ app/(pages)/signin/components/signup-form.tsx
@@ -1,25 +1,108 @@
-import { Button } from "@/app/components/ui/button";
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import { Input } from "@/app/components/ui/input";
+import { Button } from "@/app/components/ui/button";
 
 import { LockKeyholeIcon, MoveRightIcon, UserIcon } from "lucide-react";
 
-const SignUpForm = () => {
-  return (
-    <form className="w-full space-y-2.5 md:max-w-96">
-      <div className="flex flex-col gap-2.5 md:flex-row">
-        <Input icon={UserIcon} placeholder="Nome" />
+const schema = yup.object({
+  name: yup
+    .string()
+    .required("O nome é obrigatório.")
+    .matches(
+      /^[a-zA-ZÀ-ÿ]+$/,
+      "O nome não pode conter caracteres especiais ou espaços.",
+    )
+    .min(3, "Este campo precisa ter pelo menos 3 caracteres."),
+  lastName: yup
+    .string()
+    .required("O sobrenome é obrigatório.")
+    .matches(
+      /^[a-zA-ZÀ-ÿ]+$/,
+      "O sobrenome não pode conter caracteres especiais ou espaços.",
+    )
+    .min(3, "Este campo precisa ter pelo menos 3 caracteres."),
+  username: yup
+    .string()
+    .required("O nome de usuário é obrigatório.")
+    .matches(
+      /^[a-zA-Z0-9._]+$/,
+      "O nome de usuário só pode conter letras, números, ' . ' ou ' _ '.",
+    )
+    .min(6, "Este campo precisa ter pelo menos 6 caracteres."),
+  password: yup
+    .string()
+    .required("A senha é obrigatório.")
+    .min(6, "Este campo precisa ter pelo menos 6 caracteres."),
+  passwordConfirmation: yup
+    .string()
+    .required("Por favor, confirme sua senha.")
+    .oneOf([yup.ref("password")], "As senhas precisam ser iguais."),
+});
 
-        <Input icon={UserIcon} placeholder="Sobrenome" />
+type FormData = yup.InferType<typeof schema>;
+
+const SignUpForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full space-y-2.5 md:max-w-96"
+    >
+      <div className="flex flex-col gap-2.5 md:flex-row">
+        <Input
+          icon={UserIcon}
+          placeholder="Nome"
+          {...register("name")}
+          error={errors.name}
+        />
+
+        <Input
+          icon={UserIcon}
+          placeholder="Sobrenome"
+          {...register("lastName")}
+          error={errors.lastName}
+        />
       </div>
 
-      <Input icon={UserIcon} placeholder="Nome de usuário" />
-
-      <Input icon={LockKeyholeIcon} type="password" placeholder="Senha" />
+      <Input
+        icon={UserIcon}
+        placeholder="Nome de usuário"
+        {...register("username")}
+        error={errors.username}
+      />
 
       <Input
         icon={LockKeyholeIcon}
         type="password"
-        placeholder="Confirmação de email"
+        placeholder="Senha"
+        {...register("password")}
+        error={errors.password}
+      />
+
+      <Input
+        icon={LockKeyholeIcon}
+        type="password"
+        placeholder="Confirmação de senha"
+        {...register("passwordConfirmation")}
+        error={errors.passwordConfirmation}
       />
 
       <Button className="w-full justify-between uppercase">
--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -3,27 +3,32 @@ import * as React from "react";
 import { cn } from "@/app/lib/utils";
 
 import { LucideIcon } from "lucide-react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends React.ComponentProps<"input"> {
   icon?: LucideIcon;
+  error?: FieldError | undefined;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, icon: Icon, ...props }, ref) => {
+  ({ className, type, icon: Icon, error, ...props }, ref) => {
     return (
-      <div className="relative flex items-center">
-        {Icon && (
-          <Icon size={16} className="absolute left-3 text-muted-foreground" />
-        )}
-        <input
-          type={type}
-          className={cn(
-            `flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${Icon && "pl-9"}`,
-            className,
+      <div className="w-full space-y-1">
+        <div className="relative flex items-center">
+          {Icon && (
+            <Icon size={16} className="absolute left-3 text-muted-foreground" />
           )}
-          ref={ref}
-          {...props}
-        />
+          <input
+            type={type}
+            className={cn(
+              `flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${Icon && "pl-9"} ${error && "border-red-600 focus-visible:ring-red-600"}`,
+              className,
+            )}
+            ref={ref}
+            {...props}
+          />
+        </div>
+        {error && <p className="text-xs text-red-600">{error.message}</p>}
       </div>
     );
   },
